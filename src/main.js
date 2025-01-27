33b67187300e93fd9fb9798730fc799e09bad036
Miguelkoro <miguelkoro@gmail.com>
2025-01-27 17:46:15 +0100

    pc nuevo y escritorio

--- src/main.js
+++ src/main.js
@@ -30,12 +30,13 @@ const texture = new THREE.CanvasTexture(canvas);  // Usamos el canvas como textu
 texture.minFilter = THREE.LinearFilter;  // Asegurarse de que la textura se vea bien al hacer zoom
 
 // Crear un plano y aplicarle la textura
-const geometry = new THREE.PlaneGeometry(1.22, 1.05);  // Un plano de tamaño 2x2
+const geometry = new THREE.PlaneGeometry(2, 1.8);  // Un plano de tamaño 2x2
 const material = new THREE.MeshBasicMaterial({ map: texture });  // Aplicar la textura del canvas
 const plane = new THREE.Mesh(geometry, material);
 //geometry.rotateY(Math.PI);  // Rotar el plano para que se vea de frente
-plane.position.set(-0.02, 0.32, 0.6);  // Colocar el plano más cerca de la cámara o en la posición deseada
-plane.rotation.x = -0.09;  // Rotar el plano
+plane.position.set(-1.15, 1.45, -0.5);  // Colocar el plano más cerca de la cámara o en la posición deseada
+plane.rotation.x = -0.0815;  // Rotar el plano
+plane.rotation.y = 0.18;  // Rotar el plano
 // Añadir el plano a la escena
 //scene.add(plane);
 // Crear el grupo que contendrá el plano y el modelo
@@ -53,7 +54,9 @@ ctx.fillText('Contenido del Canvas', 50, 50);
 
 //------------------
 
-camera.position.z = 2; //Move the camera back
+camera.position.z = 4; //Move the camera back
+camera.position.y = 3; //Move the camera back
+camera.rotation.x = -0.5; //Tilt the camera down
 
 
 
@@ -62,12 +65,30 @@ const loader = new GLTFLoader();
 let model;  
 
 loader.load(
-  'resources/90s_computer/scene.gltf',
+  'resources/retro_computer_setup/scene.gltf',
   (gltf) => {
       model = gltf.scene;
-      model.rotation.y = 4.7; // Rotar el modelo
+      //model.rotation.y = 4.7; // Rotar el modelo
+      model.scale.set(0.05, 0.05, 0.05); // Escalar el modelo
       //scene.add(model); // Agregar el modelo a la escena
       group.add(model);  // Añadir el plano al grupo
+  },
+  undefined,
+  (error) => {
+      console.error('Error al cargar el modelo:', error);
+  }
+);
+
+let model2;
+loader.load(
+  'resources/office_table/scene.gltf',
+  (gltf) => {
+      model2 = gltf.scene;
+      //model.rotation.y = 4.7; // Rotar el modelo
+      model2.scale.set(5, 5, 5); // Escalar el modelo
+      model2.position.set(0.6, -3.8, 0);  // Colocar el modelo en la posición deseada
+      //scene.add(model); // Agregar el modelo a la escena
+      group.add(model2);  // Añadir el plano al grupo
   },
   undefined,
   (error) => {
@@ -97,12 +118,13 @@ function animate() {
     requestAnimationFrame(animate); //Call the animate function
     if (model){
       //model.rotation.y += 0.01; //Rotate the mesh
-      group.rotation.y += 0.002; //Rotate the mesh
+      //group.rotation.y += 0.002; //Rotate the mesh
       //model.rotation.y += 0.01; //Rotate the mesh
     }
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);  // Limpiar el canvas
     ctx.fillText('Texto actualizado en el Canvas', 50, 50);  // Dibujo dinámico en el canvas
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(imgPcWall, 0, 0);
     // Indicar a Three.js que la textura del canvas ha cambiado
     texture.needsUpdate = true;
