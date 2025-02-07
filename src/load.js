9b0af02bfacaa072d5a1ca3f8263739d3b756618
Miguelkoro <miguelkoro@gmail.com>
2025-02-07 10:33:52 +0100

    no message

---
+++ src/load.js
@@ -0,0 +1,42 @@
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+class Load{
+    //loadingManager; 
+    constructor(){
+       
+    //const loader = new GLTFLoader();
+    }
+
+    loadingManager() {
+        var loadingManager = new THREE.LoadingManager();
+        const loader = new GLTFLoader(loadingManager);
+        loadingManager.onStart = function (url, itemsLoaded, total) {
+            console.log('Loading process has started!');
+        };
+            loadingManager.onProgress = function (url, itemsLoaded, total) {
+            console.log(`Started loading: ${url}
+                            number of items loaded: ${itemsLoaded}
+                            total number of items: ${total}
+                        `);
+            };
+            loadingManager.onLoad = function () {
+            console.log('Loading process has been completed!');
+            };
+        
+            const progressBar = document.getElementById('progress-bar');
+        
+            loadingManager.onProgress = function (url, loaded, total) {
+            progressBar.value = (loaded / total) * 100;
+            };
+            const progressBarContainer = document.querySelector('.progress-bar-container');
+        
+            loadingManager.onLoad = function () {
+            progressBarContainer.style.display = 'none';
+            };
+            return loader;
+    }
+    
+
+    
+};
+export default Load;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 //import * as THREE from 'https://unpkg.com/three@latest/build/three.module.js'
 // npx vite
 import * as THREE from 'three';
+import Load from '/src/load.js'; 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+
 //import { TextureLoader } from 'three';
 //import { RGBELoader } from 'three/examples/js/loaders/RGBELoader.js';  // Importar RGBELoader
 
@@ -140,10 +144,37 @@ group.add( wallMesh );
 
 
 //------------------
+/*const loadingManager = new THREE.LoadingManager();
 
-const loader = new GLTFLoader();
+const loader = new GLTFLoader(loadingManager);
+//const loader = new GLTFLoader();
 
+loadingManager.onStart = function (url, itemsLoaded, total) {
+  console.log('Loading process has started!');
+};
+loadingManager.onProgress = function (url, itemsLoaded, total) {
+  console.log(`Started loading: ${url}
+                number of items loaded: ${itemsLoaded}
+                total number of items: ${total}
+              `);
+};
+loadingManager.onLoad = function () {
+  console.log('Loading process has been completed!');
+};
 
+const progressBar = document.getElementById('progress-bar');
+
+loadingManager.onProgress = function (url, loaded, total) {
+progressBar.value = (loaded / total) * 100;
+};
+const progressBarContainer = document.querySelector('.progress-bar-container');
+
+loadingManager.onLoad = function () {
+progressBarContainer.style.display = 'none';
+};*/
+const loadjs = new Load();
+const loader = loadjs.loadingManager();
+//------------------
 
 let officeLamp_model;  
 loader.load(
